209345c97a6d048764b956cf6c43528ba7bba98d
CJ van den Berg <cj@vdbonline.com>
2023-11-09 20:29:53 +0100

    Update to latest zig build API

--- build.zig
+++ build.zig
@@ -24,40 +24,43 @@ pub fn build(b: *std.build.Builder) void {
     };
 
     lib.linkLibC();
-    lib.addCSourceFiles(&.{
-        "lapi.c",
-        "lcode.c",
-        "lctype.c",
-        "ldebug.c",
-        "ldo.c",
-        "ldump.c",
-        "lfunc.c",
-        "lgc.c",
-        "llex.c",
-        "lmem.c",
-        "lobject.c",
-        "lopcodes.c",
-        "lparser.c",
-        "lstate.c",
-        "lstring.c",
-        "ltable.c",
-        "ltm.c",
-        "lundump.c",
-        "lvm.c",
-        "lzio.c",
-        "lauxlib.c",
-        "lbaselib.c",
-        "lcorolib.c",
-        "ldblib.c",
-        "liolib.c",
-        "lmathlib.c",
-        "loadlib.c",
-        "loslib.c",
-        "lstrlib.c",
-        "ltablib.c",
-        "lutf8lib.c",
-        "linit.c",
-    }, &flags);
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "lapi.c",
+            "lcode.c",
+            "lctype.c",
+            "ldebug.c",
+            "ldo.c",
+            "ldump.c",
+            "lfunc.c",
+            "lgc.c",
+            "llex.c",
+            "lmem.c",
+            "lobject.c",
+            "lopcodes.c",
+            "lparser.c",
+            "lstate.c",
+            "lstring.c",
+            "ltable.c",
+            "ltm.c",
+            "lundump.c",
+            "lvm.c",
+            "lzio.c",
+            "lauxlib.c",
+            "lbaselib.c",
+            "lcorolib.c",
+            "ldblib.c",
+            "liolib.c",
+            "lmathlib.c",
+            "loadlib.c",
+            "loslib.c",
+            "lstrlib.c",
+            "ltablib.c",
+            "lutf8lib.c",
+            "linit.c",
+        },
+        .flags = &flags,
+    });
     b.installArtifact(lib);
     lib.installHeader("lapi.h", "lapi.h");
     lib.installHeader("lauxlib.h", "lauxlib.h");
@@ -93,9 +96,7 @@ pub fn build(b: *std.build.Builder) void {
         .target = target,
         .optimize = optimize,
     });
-    exe.addCSourceFiles(&.{
-        "lua.c",
-    }, &flags);
+    exe.addCSourceFiles(.{ .files = &.{"lua.c"}, .flags = &flags });
     exe.linkLibrary(lib);
     exe.linkLibC();
     b.installArtifact(exe);
